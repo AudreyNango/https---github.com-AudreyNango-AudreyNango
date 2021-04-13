6d4cf1393a988704d519b7d53201ab402a0b1f5f
Audrey Nango <nangoaudrey@gmail.com>
2021-04-13 10:58:34 +0200

    Version 1.0

---
+++ Exo 1.js
@@ -0,0 +1,81 @@
+// var maVar ="ma valeur";
+// (function(){
+//     var maVar = "Valeur locale";
+//     console.log(maVar);
+// })();
+
+
+// function reverseTab(tab) {
+// let res = []
+// for (let i = tab.lenght -1; i>-1; i--) {
+//     res.push
+// }
+// }
+
+
+// function tirageAuSort (nom) {
+// for 
+// }
+
+var soldes = "Toyota"; ​
+
+function carTypes(nom) { ​
+
+return (nom === "Honda") ? ​
+
+nom : "Désolé, nous ne vendons pas de " + nom + "." ; ​
+
+} 
+
+console.log(carTypes(rover))​;
+
+
+// var voiture = { maVoiture: "Saturn", getVoiture: carTypes("Honda"), spécial: soldes }; ​
+
+// console.log(voiture.maVoiture); // Saturn ​
+
+// console.log(voiture.getVoiture); // Honda ​
+
+// console.log(voiture.spécial);  // Toyota
+
+//exo 1
+function carre (b){
+return (b*b)
+
+}
+
+console.log (carre(2))
+console.log (carre(4))
+
+//exo2 
+function somme (a, b) {
+if a+b < 100 
+return TRUE 
+}
+
+console.log (somme(10, 20))
+
+//ex03
+function egalite (nbr1, nbr2) {
+if nbr1 === nbr2 
+return TRUE 
+}
+console.log (egalite(48, 52))
+
+//exo4
+function inverse (tab)
+{
+    for (let i = tab.lenght-1, i < tab.lenght, i--)
+        {
+            tab(i)=tab(tab.lenght-1)
+        }
+}
+
+//exo5
+function tableau (tab) {
+return tab[tab.lenght-1]
+
+}
+
+console.log([1, 2, 3, 4, 5])
+console.log(["sebastien", "nael", "coco"])
